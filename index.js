b7136725f9923836065855c9929536f0a315c3c5
George Geils <95538913+ggeils@users.noreply.github.com>
2022-02-24 02:22:40 -0500

    added const variables for each role in index.js

---
+++ index.js
@@ -0,0 +1,8 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const Engineer = require("./lib/engineer.js");
+const Intern = require("./lib/intern.js");
+const Manager = require("./lib/manager.js");
+
+const employees = [];
+
